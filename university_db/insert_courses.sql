e27832b5e98f7fe3c3c2c601283182c69e018d33
xyrongavilla <rongavillaxyrll_bsit@plmun.edu.ph>
2025-02-28 18:24:56 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 05

---
+++ university_db/insert_courses.sql
@@ -0,0 +1,10 @@
+USE `UniversityDB`;
+
+-- Step 1: Insert data into courses table
+INSERT INTO `Courses` (`CourseName`)
+VALUES
+   ('Introduction to Computer Science'),
+   ('Calculus I'),
+   ('Modern World History'),
+   ('English Literature'),
+   ('Data Structures');
